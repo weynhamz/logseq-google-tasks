303bb1cc51038744a15dedf88fc41258ab9e8c3b
Weynham Zheng <weynhamz@gmail.com>
2024-04-27 13:59:23 +1000

    Push local changes to GTasks.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "@logseq/libs";
-import { IBatchBlock, PageEntity, IHookEvent } from "@logseq/libs/dist/LSPlugin";
+import { IBatchBlock, PageEntity, IHookEvent, BlockEntity } from "@logseq/libs/dist/LSPlugin";
 
 import { format } from "date-fns";
 
@@ -36,20 +36,21 @@ logseq.ready(main).catch(console.error);
 declare var gapi: any;
 
 async function syncGoogleTasks() {
-  console.log(gapi);
+  console.info("Start Syncing Google Tasks");
+
+  console.debug(gapi);
 
   await new Promise(resolve => {
     gapi.load('client', resolve);
   });
 
-  console.log(gapi.client);
+  console.debug(gapi.client);
 
   if (!logseq.settings?.access_token) {
     throw new Error("Access token is not set.");
   }
 
-  let tokenJson = '{"access_token":"' + logseq.settings.access_token + '"}';
-  let token = JSON.parse(tokenJson);
+  let token = JSON.parse('{"access_token":"' + logseq.settings.access_token + '"}');
   gapi.client.setToken(token);
 
   await new Promise(resolve => {
@@ -62,9 +63,9 @@ async function syncGoogleTasks() {
 
   let tasksArray = (await Promise.all(taskLists.map(
     async (taskList: any) => {
-      let tasks = await fetchTasks(taskList.id);
+      let tasks = await fetchTasks(taskList.id) ?? [];
 
-      return tasks?.map((task: any) => {
+      return tasks.map((task: any) => {
         return [taskList, task];
       });
     }
@@ -83,30 +84,18 @@ async function syncGoogleTasks() {
 
     if (res && res.length > 0) {
       if (res[0].properties["googleTaskUpdated"] === task.updated) {
+        // Here we only try to update GTasks if updated time is the same
+        // If GTasks is newer, and there is also local changes, local changes
+        // will be discarded as Logseq currently doesn't recored change date
+        // on block level reliably.
+        pushLocalChanges(res[0], task);
         continue;
       }
 
-      console.log(`Update block: ${res[0].uuid} with task: ${task.id}`);
-
-      let block = await blockContentGenerate(list, task);
-
-      logseq.Editor.updateBlock(res[0].uuid, block.content, { properties: block.properties });
-
-      // Handle notes and links update
-      let res1 = await logseq.DB.datascriptQuery(`[:find (pull ?b [*]) :where [?b :block/parent ?a] [?a :block/uuid ?uuid] [(str ?uuid) ?str] [(= ?str "${res[0].uuid}")]]`);
-      if (res1) {
-        for (let child of res1) {
-          if (child[0].properties["google-task-context"] === "notes" || child[0].properties["google-task-context"] === "links") {
-            logseq.Editor.removeBlock(child[0].uuid);
-          }
-        }
-      }
-      if (block.children && block.children.length > 0) {
-        logseq.Editor.insertBatchBlock(res[0].uuid, block.children, {sibling: false});
-      }
+      updateTaskBlock(res[0], list, task);
     }
     else {
-      console.log(`Insert block for task: ${task.id}`);
+      console.info(`Insert block for task: ${task.id}`);
 
       let parentName = await generateParentName(list, task);
 
@@ -155,7 +144,7 @@ async function fetchTaskLists(): Promise<gapi.client.tasks.TaskList[] | undefine
 /**
  * Fetches tasks from the Google Tasks API for a given task list ID.
  * @param taskListId - The ID of the task list.
- * @returns {Promise<any[]>} A promise that resolves to an array of tasks.
+ * @returns A promise that resolves to an array of tasks.
  */
 async function fetchTasks(taskListId: string): Promise<gapi.client.tasks.Task[] | undefined> {
   let tasks: any[] = [];
@@ -194,7 +183,7 @@ async function fetchTasks(taskListId: string): Promise<gapi.client.tasks.Task[] 
  * Ensures the existence of a page in Logseq.
  * If the page doesn't exist, it creates a new page with the specified name.
  * If the page already exists, it returns the existing page entity.
- * 
+ *
  * @param page - The name of the page to ensure.
  * @param isJournal - Optional. Specifies whether the page is a journal page. Default is false.
  * @returns A Promise that resolves to the PageEntity of the page, or null if the page couldn't be created.
@@ -208,7 +197,47 @@ async function ensurePage(page: string, isJournal: boolean = false): Promise<Pag
 }
 
 /**
+ * Updates a task block with the provided task information.
+ * @param block - The block entity representing the task block.
+ * @param list - The task list object or its ID.
+ * @param task - The task object.
+ */
+async function updateTaskBlock(block: BlockEntity, list: gapi.client.tasks.TaskList | string, task: gapi.client.tasks.Task) {
+  console.info(`Update block: ${block.uuid} with task: ${task.id} : ${task.title}`);
+  console.debug('Remote' + task.updated);
+  console.debug(task);
+  console.debug('Local ' + block.properties?.["googleTaskUpdated"]);
+  console.debug(block);
+
+  // Get list object if list is a string
+  if (typeof list === 'string') {
+    list = (await gapi.client.tasks.tasklists.get({ tasklist: list })).result as gapi.client.tasks.Task;
+  }
+
+  let blockNew = await blockContentGenerate(list, task);
+
+  console.debug(blockNew);
+
+  await logseq.Editor.updateBlock(block.uuid, blockNew.content, { properties: blockNew.properties });
+
+  // Handle notes and links update
+  let res: any = await logseq.DB.datascriptQuery(`[:find (pull ?b [*]) :where [?b :block/parent ?a] [?a :block/uuid ?uuid] [(str ?uuid) ?str] [(= ?str "${block.uuid}")]]`);
+  if (res) {
+    console.debug(res);
+    for (let child of res) {
+      if (child[0].properties["google-task-context"] === "notes" || child[0].properties["google-task-context"] === "links") {
+        await logseq.Editor.removeBlock(child[0].uuid);
+      }
+    }
+  }
+  if (blockNew.children && blockNew.children.length > 0) {
+    await logseq.Editor.insertBatchBlock(block.uuid, blockNew.children, { sibling: false });
+  }
+}
+
+/**
  * Generates a batch block based on the provided task.
+ * @param list - The task list object.
  * @param task - The task object.
  *   {
  *     "kind": string,
@@ -235,9 +264,6 @@ async function ensurePage(page: string, isJournal: boolean = false): Promise<Pag
  *     "webViewLink": string
  *   }
  * @returns A promise that resolves to the generated batch block.
- *
- * @TODO handle parent relationship
- * @TODO handle repeat of the deadline
  */
 async function blockContentGenerate(list: gapi.client.tasks.TaskList, task: gapi.client.tasks.Task): Promise<IBatchBlock> {
   const { preferredDateFormat, preferredTodo } = await logseq.App.getUserConfigs();
@@ -260,7 +286,8 @@ async function blockContentGenerate(list: gapi.client.tasks.TaskList, task: gapi
 
   taskBlock.properties = {};
   taskBlock.properties["google-task-id"] = task.id;
-  taskBlock.properties["google-task-list"] = `[[GTasks/${list.title}]]`;
+  taskBlock.properties["google-task-list-id"] = list.id;
+  taskBlock.properties["google-task-list-ref"] = `[[GTasks/${list.title}]]`;
   taskBlock.properties["google-task-updated"] = task.updated;
   taskBlock.properties["google-task-webViewLink"] = task.webViewLink;
   if (task.hidden && task.status !== 'completed') {
@@ -344,4 +371,98 @@ async function generateParentName(list: gapi.client.tasks.TaskList, task: gapi.c
   }
 
   return `${format(earliestDate, preferredDateFormat)}`;
+}
+
+/**
+ * Pushes local changes to a Google Tasks task based on a Logseq block.
+ * @param block - The Logseq block representing the task.
+ * @param task - The Google Tasks task to be updated.
+ * @returns A Promise that resolves when the task has been updated.
+ */
+async function pushLocalChanges(block: BlockEntity, task: gapi.client.tasks.Task) {
+  let needsUpdate = false;
+  let taskNew = { ...task };
+  let taskTitle = block.content
+    .replace(/^(DONE|TODO)? /, '')
+    .replace(/\nDEADLINE: [^\n]*/g, '')
+    .replace(/\n[^\n]*:: [^\n]*/g, '')
+    .replace(/^[^\n]*:: [^\n]*\n/g, '');
+  if (taskTitle.trim() !== task.title?.trim()) {
+    console.debug(taskTitle);
+    console.debug(task.title);
+    taskNew.title = taskTitle;
+    needsUpdate = true;
+  }
+
+  // Handle status update
+  if (block.marker === 'DONE' && task.status === 'needsAction') {
+    taskNew.status = 'completed';
+    // Logseq by default does not have completed date recorded for tasks.
+    // This is now a feature provided by a plugin, which links to a jdournal,
+    // page, there is a possiblity that completed date is not recorded.
+    if (block.properties?.completed) {
+      taskNew.completed = format(new Date((block.properties.completed || '').replace(/\[\[|\]\]/g, '')), 'yyyy-MM-dd') + 'T00:00:00.000Z';
+    }
+    needsUpdate = true;
+  }
+  if (block.marker === 'TODO' && task.status === 'completed') {
+    taskNew.status = 'needsAction';
+    delete taskNew.completed;
+    needsUpdate = true;
+  }
+
+  // Handle deadline, format is 20240202
+  if (block.deadline) {
+    let deadlineFormatted = block.deadline.toString().slice(0, 4) + '-' + block.deadline.toString().slice(4, 6) + '-' + block.deadline.toString().slice(6, 8);
+    if (format(new Date(deadlineFormatted), 'yyyy-MM-dd') !== format(new Date(task.due as string), 'yyyy-MM-dd')) {
+      console.debug(format(new Date(deadlineFormatted), 'yyyy-MM-dd') + 'T00:00:00.000Z');
+      console.debug(task.due);
+      taskNew.due = format(new Date(deadlineFormatted), 'yyyy-MM-dd') + 'T00:00:00.000Z';
+      needsUpdate = true;
+    }
+  }
+
+  // todo handle notes
+  // Don't hanlde notes for now, as Logseq does not properly hanlde notes with newlines started '-'
+  // Some imported notes are incomplete, we can't really tell if it is a bad import or a local update
+  //let res = await logseq.DB.datascriptQuery(`[:find (pull ?b [*]) :where [?b :block/parent ?a] [?a :block/uuid ?uuid] [(str ?uuid) ?str] [(= ?str "${block.uuid}")]]`);
+  //if (res) {
+  //  for (let child of res) {
+  //    if (child[0].properties["google-task-context"] === "notes") {
+  //      let taskNotes = child[0].content
+  //        .replace(/^(DONE|TODO)? /, '')
+  //        .replace(/\nDEADLINE: [^\n]*/g, '')
+  //        .replace(/\n[^\n]*:: [^\n]*/g, '')
+  //        .replace(/^[^\n]*:: [^\n]*\n/g, '');
+  //      if (taskNotes.trim() !== task.notes?.trim()) {
+  //        console.debug(taskNotes);
+  //        console.debug(task.notes);
+  //        //taskNew.notes = taskNotes;
+  //        //needsUpdate = true;
+  //      }
+  //    }
+  //  }
+  //}
+
+  if (needsUpdate) {
+    console.debug(block);
+    console.debug(taskNew);
+
+    await gapi.client.tasks.tasks.update({
+      tasklist: block.properties?.["googleTaskListId"],
+      task: taskNew.id,
+      resource: taskNew,
+    });
+
+    let updatedTask = await gapi.client.tasks.tasks.get({
+      tasklist: block.properties?.["googleTaskListId"],
+      task: taskNew.id,
+    });
+
+    console.debug(updatedTask);
+
+    updateTaskBlock(block, block.properties?.["googleTaskListId"] as string, updatedTask.result as gapi.client.tasks.Task);
+
+    console.info(`Task ${task.id} has been changed locally`);
+  }
 }
