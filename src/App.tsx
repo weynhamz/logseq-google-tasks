ac99946af4c2bf8218b2fc93c721c03bef183963
Weynham Zheng <weynhamz@gmail.com>
2025-03-10 19:22:53 +1100

    feat: set up initial layout of the plugin main UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "virtual:uno.css";
 
 function App() {
   return (
-    <main>Logseq Google Tasks</main>
+    <main className="flex w-screen h-screen items-center justify-center absolute top-0 left-0 right-0 bottom-0 bg-gray-500 opacity-90">
+      <div id="innerDiv" className="rounded-lg shadow-lg relative text-center text-xl p-4 border bg-gray-100 border-gray-300 w-3/4 h-auto">
+        <div>Logseq Google Tasks</div>
+      </div>
+    </main>
   );
 }
 
